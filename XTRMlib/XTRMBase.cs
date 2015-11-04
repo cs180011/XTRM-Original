31bbbe8a96fa5ef0c1bc164ee99bef63db8f65dd
Chris Swift <chris.swift@jmp.com>
2015-11-04 15:27:06 -0500

    Minor Revision

--- XTRMlib/XTRMBase.cs
+++ XTRMlib/XTRMBase.cs
@@ -14,7 +14,7 @@ namespace XTRMlib
 {
     public class XTRMBase : Object
     {
-        protected SqlConnection XConnection = null;     // sample3
+        protected SqlConnection XConnection = null;     // sample4
         protected static SqlConnection XTRMAPP = null;
         protected static int lLogID = 0;  // Default
         //public string className;
